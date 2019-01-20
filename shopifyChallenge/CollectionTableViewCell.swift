90a20d1925de8f4ee5554e50656df48b72bc503f
Bruno Alexander Cremonese de Morais <brualexander@gmail.com>
2019-01-19 22:36:58 -0500

    Collection list and segue implemented

---
+++ shopifyChallenge/CollectionTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  CollectionTableViewCell.swift
+//  shopifyChallenge
+//
+//  Created by Bruno Alexander Cremonese de Morais on 2019-01-19.
+//  Copyright © 2019 Bruno Alexander Cremonese de Morais. All rights reserved.
+//
+
+import UIKit
+
+class CollectionTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var collectionTitle: UILabel!
+    @IBOutlet weak var collectionThumbnail: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ shopifyChallenge/CollectionsTableViewController.swift
@@ -0,0 +1,72 @@
+//
+//  ViewController.swift
+//  shopifyChallenge
+//
+//  Created by Bruno Alexander Cremonese de Morais on 2019-01-17.
+//  Copyright © 2019 Bruno Alexander Cremonese de Morais. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+import AlamofireMapper
+
+class CollectionsTableViewController: UITableViewController {
+    
+    var customCollections : CustomCollections?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.title = "Collections"
+        self.tableView?.rowHeight = 90.0;
+        
+        //Retrieve custom_collections from the API and set self.customCollections to the response value
+        Alamofire.request("https://shopicruit.myshopify.com/admin/custom_collections.json?page=1&access_token=c32313df0d0ef512ca64d5b336a0d7c6").responseObject { (response: DataResponse<CustomCollections>) in
+            
+            self.customCollections = response.result.value
+            self.tableView.reloadData()
+        }
+    }
+    
+    // MARK: - Table view building
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.customCollections?.custom_collections.count ?? 0
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? CollectionTableViewCell else{
+            
+            fatalError("Dequeue error")
+        }
+        
+        cell.collectionTitle.text = self.customCollections?.custom_collections[indexPath.row].title
+        cell.collectionThumbnail.load(url: URL(string: (self.customCollections?.custom_collections[indexPath.row].image.src)!)!)
+        
+        return cell
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        
+    }
+}
+
+extension UIImageView {
+    func load(url: URL) {
+        DispatchQueue.global().async { [weak self] in
+            if let data = try? Data(contentsOf: url) {
+                if let image = UIImage(data: data) {
+                    DispatchQueue.main.async {
+                        self?.image = image
+                    }
+                }
+            }
+        }
+    }
+}
